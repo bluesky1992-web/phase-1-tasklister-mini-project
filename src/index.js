55c3ec4a86fdec4d102cf1a4018f8e32efd28087
Ali ibrahim jasim <ali.ibrahim.jasim@gmail.com>
2022-10-20 13:08:28 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById("create-task-form")
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()       //prevent the page from refreshing 
+    buildToDo(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+
+
+
+
+function buildToDo(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
